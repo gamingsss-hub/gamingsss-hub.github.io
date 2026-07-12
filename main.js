aca5360591fe1c820f84369c77babf8795339980
Gamingsss-hub <yt3766701@gmail.com>
2026-07-12 14:22:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1138,3 +1138,23 @@ behavior:"smooth"
 });
 
 };
+
+// Loader
+
+window.addEventListener("load",()=>{
+
+const loader=document.getElementById("loader");
+
+if(loader){
+
+loader.style.opacity="0";
+
+setTimeout(()=>{
+
+loader.style.display="none";
+
+},600);
+
+}
+
+});
